538488300097bcf19fd7c5e2c20f2cefd66c3b20
Marina Portugal <marinapsvreis@gmail.com>
2022-05-12 17:00:59 -0300

    implementacao tela login adm

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function activeMenu() {
 }
 
 menuHamburguer.addEventListener('click', activeMenu)
+
+function logar() {
+  var login = document.getElementById('login').value
+  var senha = document.getElementById('senha').value
+
+  if (login == 'admin' && senha == 'admin') {
+    alert('Sucesso!')
+    location.href = './pedidos.html'
+  } else {
+    alert('Usuário ou senha incorretos')
+  }
+}
